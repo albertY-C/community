cd821f8c99bef1afe9bc62be9a6ec636588d58d3
albertY-C <chaomi_yc@163.com>
2020-03-15 11:03:41 +0800

    Add check cookies

--- src/main/java/chaoimi/community/controller/IndexController.java
+++ src/main/java/chaoimi/community/controller/IndexController.java
@@ -1,10 +1,34 @@
 package chaoimi.community.controller;
 
+import chaoimi.community.mapper.UserMapper;
+import chaoimi.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Autowired(required = false)
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){ return "index"; }
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if( cookie.getName().equals("token") ){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        return "index";
+    }
 }
